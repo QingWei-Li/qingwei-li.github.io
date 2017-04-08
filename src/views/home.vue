70573199ecec6c8eb47f7f07dcd7570aafe913ef
qingwei.li <cinwell.li@gmail.com>
2017-04-08 12:13:45 +0800

    init

---
+++ src/views/home.vue
@@ -0,0 +1,107 @@
+<template>
+  <main>
+    <section class="profile">
+      <img
+        class="profile__avatar"
+        src="https://github.com/qingwei-li.png"
+        alt="qingwei-li">
+      <h1 class="profile__name">Qingwei Li</h1>
+      <blockquote class="profile__description">A Web developer who loves to create amazing things</blockquote>
+      <ul class="links">
+        <li class="links__item"><a href="http://weibo.com/ihermit">Weibo</a></li>
+        <li class="links__item"><a href="https://www.zhihu.com/people/cinwell">Zhihu</a></li>
+        <li class="links__item"><a href="https://github.com/qingwei-li">GitHub</a></li>
+      </ul>
+    </section>
+
+   <!--  <section class="list">
+      <h4>My projects</h4>
+      <ul class="projects">
+      </ul>
+    </section> -->
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+
+    head: {
+      title: 'Qingwei Li',
+      link: [
+        { rel: 'favicon', href: 'https://github.com/qingwei-li.png' }
+      ]
+    }
+  }
+</script>
+
+<style>
+  @import url(//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600);
+
+  body {
+    font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
+    color: #2c3e50;
+    max-width: 800px;
+    margin: auto;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  .profile {
+    text-align: center;
+    /*padding-top: 10vh;*/
+    padding-top: 30vh;
+    box-sizing: border-box;
+  }
+
+  .profile__avatar {
+    border-radius: 50%;
+    height: 8rem;
+    width: 8rem;
+  }
+
+  .profile__name {
+    margin: 1rem 0;
+  }
+
+  .profile__description {
+    color: #7f8c8d;
+    margin: 1rem 4rem;
+  }
+
+  .links {
+    text-align: center;
+  }
+
+  .links__item {
+    display: inline-block;
+    padding: 0 .4rem;
+  }
+
+  .list {
+    text-align: center;
+    margin-top: 5rem;
+  }
+
+ /* @media screen and (max-width: 600px) {
+    .profile {
+      height: 100vh;
+      padding-top: 26vh;
+    }
+
+    .list {
+      margin-top: 0;
+    }
+  }*/
+</style>
