7fd6d40bcc43f90fb154c05f1f8325b86da6cdea
qingwei.li <cinwell.li@gmail.com>
2018-02-12 08:14:35 +0800

    fix links

--- src/components/project.vue
+++ src/components/project.vue
@@ -6,9 +6,9 @@
       <p class="project__desc">{{ data.desc }}</p>
       <ul class="project__links">
         <li>
-          <a target="_blank" :href="'https://github.com/qingwei-li/' + data.name">GitHub</a>
+          <a target="_blank" :href="data.name | repo">GitHub</a>
         </li>
-        <li v-for="link in data.links">
+        <li v-for="link in data.links" :key="link.name">
           <a target="_blank" :href="link.url">{{ link.name }}</a>
         </li>
       </ul>
@@ -17,70 +17,78 @@
 </template>
 
 <script>
-  import zoom from 'zoom-image'
+import zoom from 'zoom-image';
 
-  export default {
-    name: 'project',
+export default {
+  name: 'project',
 
-    props: ['data'],
+  props: ['data'],
 
-    mounted() {
-      this.destroyZoom = zoom(this.$refs.image)
-    },
+  filters: {
+    repo(name) {
+      return 'https://github.com/' + name.indexOf('/') === -1
+        ? 'qingwei-li' + name
+        : name;
+    }
+  },
 
-    beforeDestroy() {
-      this.destroyZoom()
-    },
+  mounted() {
+    this.destroyZoom = zoom(this.$refs.image);
+  },
 
-    methods: {
-      previewImage () {
-        window.open(this.data.image)
-      }
+  beforeDestroy() {
+    this.destroyZoom();
+  },
+
+  methods: {
+    previewImage() {
+      window.open(this.data.image);
     }
   }
+};
 </script>
 
 <style src="zoom-image/css/zoom-image.css"></style>
 <style>
-  .project {
-    display: flex;
-    text-align: left;
-    align-items: center;
-  }
+.project {
+  display: flex;
+  text-align: left;
+  align-items: center;
+}
 
+.project__image {
+  width: 100px;
+}
+
+@media screen and (max-width: 600px) {
   .project__image {
-    width: 100px;
+    width: 0;
   }
+}
 
-  @media screen and (max-width: 600px) {
-    .project__image {
-      width: 0;
-    }
-  }
+.project__info {
+  flex: 1;
+  margin-left: 20px;
+}
 
-  .project__info {
-    flex: 1;
-    margin-left: 20px;
-  }
+.project__name {
+  font-size: 20px;
+  margin-bottom: 0.5rem;
+}
 
-  .project__name {
-    font-size: 20px;
-    margin-bottom: .5rem;
-  }
+.project__desc {
+  color: #7f8c8d;
+}
 
-  .project__desc {
-    color: #7f8c8d;
-  }
+.project__links {
+  margin-top: 0.5rem;
+}
 
-  .project__links {
-    margin-top: .5rem;
-  }
+.project__links li {
+  display: inline-block;
+}
 
-  .project__links li {
-    display: inline-block;
-  }
-
- .project__links a {
-    padding: 0 .4rem;
- }
+.project__links a {
+  padding: 0 0.4rem;
+}
 </style>
